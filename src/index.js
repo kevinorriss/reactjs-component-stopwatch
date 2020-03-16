619569f230b6cfbdc9096c0ebccf58e742e827c2
Kevin Orriss <kevinorriss@gmail.com>
2020-03-16 00:05:03 +0000

    v1.0.1
    Fixed style issue with the laps container

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Stopwatch from '@kevinorriss/stopwatch'
 
-ReactDOM.render(<Stopwatch/>, document.getElementById('root'))
+ReactDOM.render(
+    <div className="card" style={{marginTop: '2rem', height: '30rem'}}>
+        <div className="card-body">
+            <Stopwatch />
+        </div>
+    </div>,
+    document.getElementById('root')
+)
